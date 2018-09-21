ff692bcddde13a5c587ffcd507e0a27a656eb7cb
David <davidsherrier1997@gmail.com>
2018-09-20 21:23:48 -0400

    remove_node and remove_edge working, updated exceptions

--- graph.hpp
+++ graph.hpp
@@ -1,49 +1,32 @@
-#ifndef _GRAPH_HPP
-#define _GRAPH_HPP
+#ifndef _GRAPH_BASE_HPP
+#define _GRAPH_BASE_HPP
 
+#include <deque>
 #include <cstdint>
 #include <functional>
 #include <initializer_list>
 #include <iterator>
-#include <limits>
 #include <map>
+#include <vector>
 #include <string>
-#include <set>
 #include <type_traits>
 #include <utility>
 #include <exception>
-
-// TODO
-// remove_node and remove_edge functions
-// Add directed_graph and undirected_graph classes that inherit from graph_base
-// Allow all graph types to be weighted
-// Add type trait checks where necessary
-// Add static_asserts
-// Update exception class
-
-class GraphError {
-	private:	
-		const std::string error;
-	public:
-		GraphError(const std::string& _error) : error(_error) {}
-		const char* what() const noexcept {
-			return error.c_str();
-		}
-};
+#include "graph_exception.hpp"
 
 template <class Type,
-	  class Mapped = std::set<Type>,
-	  class Container = std::map<Type, Mapped>>
+	  class Mapped = std::vector<std::pair<Type,double>>,
+	  class Container = std::map<Type, Mapped>,
+	  bool  Directed = false>
 struct graph_base
 {
-public:
-	// std::map types
+	// Container types
 	using container_type  = Container;
 	using key_type        = typename container_type::key_type;
 	using mapped_type     = typename container_type::mapped_type;
 
-	// std::set types
-	using value_type      = Type;
+	// Mapped types
+	using value_type      = typename std::pair<Type,double>;
 	using allocator_type  = typename Mapped::allocator_type;
 	using reference       = typename allocator_type::reference;
 	using const_reference = typename allocator_type::const_reference;
@@ -56,6 +39,7 @@ public:
 	using const_reverse_iterator  = typename container_type::const_reverse_iterator;
 	using mapped_const_iterator   = typename mapped_type::const_iterator;
 
+	// members
 	container_type graph_map;
 	size_type num_nodes;
 	size_type num_edges;
@@ -69,6 +53,79 @@ public:
 	// helper functions
 	bool check_key_exist(const key_type& key) {
 		return graph_map.find(key) != graph_map.end();
+	}
+	void invalidateEdges(const key_type& key) {
+		for(auto iter = graph_map.begin(); iter != graph_map.end(); ++iter) {
+			auto search = std::find_if(iter->second.begin(), iter->second.end(), [&](const value_type& element) { return element.first == key; });
+			if(search != iter->second.end()) {
+				iter->second.erase(search);
+				if (!Directed) {
+					num_edges -= 2;
+				} else {
+					--num_edges;
+				}
+			}
+		}
+	}
+	bool checkEdgeExists(const key_type& key1, const key_type& key2) {
+		const auto& neighbors_key1 = graph_map[key1];
+		auto search_key2_in_key1 = std::find_if(neighbors_key1.begin(), neighbors_key1.end(), [&](const value_type& element) { return element.first == key2; });
+		if(Directed) {
+			return search_key2_in_key1 != neighbors_key1.end();
+		} else {
+			const auto& neighbors_key2 = graph_map[key2];
+			auto search_key1_in_key2 = std::find_if(neighbors_key2.begin(), neighbors_key2.end(), [&](const value_type& element) { return element.first == key1; });
+			return search_key2_in_key1 != neighbors_key1.end() && search_key1_in_key2 != neighbors_key2.end();
+		}
+		return false;
+	}
+	bool remove_edge_directed(const key_type& key1, const key_type& key2) {
+		bool checkEdge = checkEdgeExists(key1, key2);
+		if(!checkEdge) {
+			throw EdgeNotFound("Edge not found, try adding edge first!\n");
+		}
+		auto& neighbors_key1 = graph_map[key1];
+		auto search_key2_in_key1 = std::find_if(neighbors_key1.begin(), neighbors_key1.end(), [&](const value_type& element) { return element.first == key2; });
+		if(search_key2_in_key1 != neighbors_key1.end()) {
+			neighbors_key1.erase(search_key2_in_key1);
+			--num_edges;
+		}
+		return true;
+	}
+	bool remove_edge_undirected(const key_type& key1, const key_type& key2) {
+		bool checkEdge = checkEdgeExists(key1, key2);
+		if(!checkEdge) {
+			throw EdgeNotFound("Edge not found, try adding edge first!\n");
+		}
+		auto& neighbors_key1 = graph_map[key1];
+		auto& neighbors_key2 = graph_map[key2];
+		auto search_key2_in_key1 = std::find_if(neighbors_key1.begin(), neighbors_key1.end(), [&](const value_type& element) { return element.first == key2; });
+		auto search_key1_in_key2 = std::find_if(neighbors_key2.begin(), neighbors_key2.end(), [&](const value_type& element) { return element.first == key1; });
+		if(search_key2_in_key1 != neighbors_key1.end() && search_key1_in_key2 != neighbors_key2.end()) {
+			neighbors_key1.erase(search_key2_in_key1);
+			neighbors_key2.erase(search_key1_in_key2);
+			num_edges -= 2;
+		}
+		return true;
+	}
+	bool insert_edge_directed(const key_type& key1, const key_type& key2, const double w = 1.0) {
+		bool checkEdge = checkEdgeExists(key1, key2);
+		if(checkEdge) {
+			throw EdgeExists("Edge already exists, try removing first!\n");
+		}
+		graph_map[key1].push_back({key2, w});
+		++num_edges;
+		return true;
+	}
+	bool insert_edge_undirected(const key_type& key1, const key_type& key2, const double w = 1.0) {
+		bool checkEdge = checkEdgeExists(key1, key2);
+		if(checkEdge) {
+			throw EdgeExists("Edge already exists, try removing first!\n");
+		}
+		graph_map[key1].push_back({key2, w});
+		graph_map[key2].push_back({key1, w});
+		num_edges += 2;
+		return true;
 	}
 
 	// constructors
@@ -92,7 +149,7 @@ public:
 	bool insert_node(const key_type& key) {
 		bool check = check_key_exist(key);
 		if(check) {
-			throw GraphError("Node already exists!");
+			throw NodeExists("Try removing node first!\n");
 		}
 		graph_map[key];
 		++num_nodes;
@@ -101,31 +158,55 @@ public:
 	bool insert_node(key_type&& key) {
 		bool check = check_key_exist(key);
 		if(check) {
-			throw GraphError("Node already exists!");
+			throw NodeExists("Try removing node first!\n");
 		}
 		graph_map[std::move(key)];
 		++num_nodes;
 		return true;
 	}
-	bool insert_edge(const key_type& key1, const key_type& key2) {
-		bool check_key1 = check_key_exist(key1);
-		bool check_key2 = check_key_exist(key2);
-		if(!check_key1 || !check_key2) {
-			throw GraphError("Node being used to build edge does not exist!");
+	bool insert_edge(const key_type& key1, const key_type& key2, const double w = 1.0) {
+		bool check1 = check_key_exist(key1);
+		bool check2 = check_key_exist(key2);
+		if(!check1 || !check2) {
+			throw NodeNotFound("Check node was added to the container!\n");
 		}
-		if(!graph_map[key1].insert(key2).second || !graph_map[key2].insert(key1).second) {
-			throw GraphError("Edge already exists between these nodes!");
+		if (!Directed) {
+			return insert_edge_undirected(key1, key2, w);
+		} else {
+			return insert_edge_directed(key1, key2, w);
 		}
-		num_edges += 2;
-		return true;
+		return false;
 	}
-	std::pair<mapped_const_iterator, mapped_const_iterator> out_degrees(const key_type& key) {
+	bool remove_node(const key_type& key) {
 		bool check = check_key_exist(key);
 		if(!check) {
-			throw GraphError("Node does not exist!");
+			throw NodeNotFound("Check node was added to the container!\n");
 		}
-		const auto& neighbors = graph_map[key];
-		return {neighbors.cbegin(), neighbors.cend()};
+		graph_map.erase(key);
+		--num_nodes;
+		invalidateEdges(key);
+		return true;
+	}
+	bool remove_edge(const key_type& key1, const key_type& key2) {
+		bool check1 = check_key_exist(key1);
+		bool check2 = check_key_exist(key2);
+		if(!check1 || !check2) {
+			throw NodeNotFound("Check node was added to the container!\n");
+		}
+		if(Directed) {
+			return remove_edge_directed(key1, key2);
+		} else {
+			return remove_edge_undirected(key1, key2);
+		}	
+		return false;
+	}
+	std::pair<mapped_const_iterator, mapped_const_iterator> neighbors(const key_type& key) {
+		bool check = check_key_exist(key);
+		if(!check) {
+			throw NodeNotFound("Check node was added to the container!\n");
+		}
+		const auto& neighborList = graph_map[key];
+		return {neighborList.cbegin(), neighborList.cend()};
 	}
 	bool empty() const noexcept {
 		return graph_map.empty();
@@ -133,8 +214,8 @@ public:
 	void clear() noexcept {
 		graph_map.clear();
 	}
-	size_type size() const noexcept {
-		return graph_map.size();
+	size_type size() noexcept{
+		graph_map.size();
 	}
 };
 
